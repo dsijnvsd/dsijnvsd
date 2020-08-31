abbff113af411ca50741190f854caabbc1ecce80
Wenhao Liu <809968@ACDM.DS.SAIT.CA>
2020-08-31 15:07:17 -0600

    0

--- main.php
+++ main.php
@@ -2,7 +2,7 @@
 
 <body>
 
-<form action="logn.php" method="post">
+<form action="login.php" method="post">
    Name: <input type="text" name="name"><br>
    Password: <input type="password" name="pass"><br>
    <input type="submit" value="Register">
