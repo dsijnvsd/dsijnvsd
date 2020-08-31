d5c27afc093d501fe23ab7da9f02c3d9178c5f3b
Wenhao Liu <809968@ACDM.DS.SAIT.CA>
2020-08-31 14:57:38 -0600

    0

---
+++ login.php
@@ -0,0 +1,29 @@
+<html><body>
+
+<?php
+
+	$username = $_POST["name"];
+	$password = $_POST["pass"];
+	$servername = "127.0.0.1";
+	$dbusername = "root";
+	$db = "sct";
+
+	$conn = mysqli_connect($servername, $dbusername,"", $db);
+	if ($conn->connect_error) {
+		echo "Connection Error";
+	}
+
+	$sql = "INSERT INTO login (username, pass) VALUES ('$username','$password')";
+
+	if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+	}
+	else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+	}
+
+$conn->close();
+
+?>
+</body>
+</html>
---
+++ main.php
@@ -0,0 +1,11 @@
+<html>
+
+<body>
+
+<form action="logn.php" method="post">
+   Name: <input type="text" name="name"><br>
+   Password: <input type="password" name="pass"><br>
+   <input type="submit" value="Register">
+</form>
+</body>
+</html>
