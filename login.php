5120f241d28a04866dbf6ef1adf3eb6bf9cb6944
Wenhao Liu <809968@ACDM.DS.SAIT.CA>
2020-08-31 15:05:28 -0600

    0

--- login.php
+++ login.php
@@ -4,16 +4,17 @@
 
 	$username = $_POST["name"];
 	$password = $_POST["pass"];
-	$servername = "127.0.0.1";
-	$dbusername = "root";
-	$db = "sct";
+	$servername = "192.168.0.31";
+	$dbusername = "admin";
+	$db = "wenhao";
+	$dbPassword = '20200101';
 
-	$conn = mysqli_connect($servername, $dbusername,"", $db);
+	$conn = mysqli_connect($servername, $dbusername,$dbPassword, $db);
 	if ($conn->connect_error) {
 		echo "Connection Error";
 	}
 
-	$sql = "INSERT INTO login (username, pass) VALUES ('$username','$password')";
+	$sql = "INSERT INTO testing.customers (name, address, password) VALUES ('$username','$password')";
 
 	if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
--- register.php
+++ register.php
@@ -5,11 +5,11 @@
 
 	$username = $_POST["name"];
 	$password = $_POST["pass"];
-	$servername = "127.0.0.1";
-	$dbusername = "root";
-	$db = "sct";
-
-	$conn = mysqli_connect($servername, $dbusername,"", $db);
+	$servername = "192.168.0.31";
+	$dbusername = "admin";
+	$db = "wenhao";
+	$dbPassword = '20200101';
+	$conn = mysqli_connect($servername, $dbusername,$dbPassword, $db);
 	if ($conn->connect_error) {
 		echo "Connection Error";
 	}
