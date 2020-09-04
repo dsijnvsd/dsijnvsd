04fa1ba40c749091243ee9891ea680107bc0336b
Wenhao Liu <809968@ACDM.DS.SAIT.CA>
2020-09-04 10:50:19 -0600

    0

--- login.php
+++ login.php
@@ -2,20 +2,20 @@
 
 <?php
 
-	$username = $_POST["name"];
-	$password = $_POST["pass"];
-	$servername = "192.168.0.31";
-	$dbusername = "admin";
-	$db = "testing";
-	$dbPassword = "20200101";
-	$address = '';
-	$conn = mysqli_connect($servername, $dbusername,$dbPassword, $db);
-	if ($conn->connect_error) {
-		echo "Connection Error";
-	}
+	// $username = $_POST["name"];
+	// $password = $_POST["pass"];
+	// $servername = "192.168.0.31";
+	// $dbusername = "admin";
+	// $db = "testing";
+	// $dbPassword = "20200101";
+	// $address = '';
+	// $conn = mysqli_connect($servername, $dbusername,$dbPassword, $db);
+	// if ($conn->connect_error) {
+	// 	echo "Connection Error";
+	// }
 
-	$sql = "SELECT password from testing.customers where name=$username";
-    echo $sql
+	// $sql = "SELECT password from testing.customers where name=$username";
+    // echo $sql
 	// if ($conn->query($sql) === TRUE) {
     // header('Location: http://25.29.164.31:5501/SimpleTax.html'); 
 	// }
@@ -23,7 +23,7 @@
     // echo "Please try again.";
 	// }
 
-$conn->close();
+// $conn->close();
 
 
  
