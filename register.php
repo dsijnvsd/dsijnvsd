dde29094014f23bda59900c18dd451a26cc110ad
Wenhao Liu <809968@ACDM.DS.SAIT.CA>
2020-09-03 23:10:06 -0600

    0

--- register.php
+++ register.php
@@ -3,12 +3,18 @@
 
 <?php
 
+	// $username = $_POST["name"];
+	// $password = $_POST["pass"];
+	// $servername = "192.168.0.31";
+	// $dbusername = "admin";
+	// $db = "testing";
+	// $dbPassword = '20200101';
 	$username = $_POST["name"];
 	$password = $_POST["pass"];
-	$servername = "192.168.0.31";
-	$dbusername = "admin";
-	$db = "testing";
-	$dbPassword = '20200101';
+	$servername = "localhost";
+	$dbusername = "root";
+	$db = "cprg251";
+	$dbPassword = "123456";
 	$address = '';
 	$conn = mysqli_connect($servername, $dbusername,$dbPassword, $db);
 	if ($conn->connect_error) {
