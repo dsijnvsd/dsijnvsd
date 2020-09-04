da668e0654db5061fd386ecd2f4d5fe9bebd92b2
Wenhao Liu <809968@ACDM.DS.SAIT.CA>
2020-09-04 11:05:35 -0600

    0

--- login.php
+++ login.php
@@ -14,9 +14,16 @@
 		echo "Connection Error";
 	}
 
-	$sql = "SELECT password from testing.customers where name=$username";
-	$checkpassword = query($sql);
-	echo $checkpassword;
+	// $sql = "SELECT password from testing.customers where name= '$username";
+
+	$sql= "SELECT password FROM testing.customers WHERE name = '$username' AND password = '$password' ";
+	$result = mysqli_query($con,$sql);0
+	$check = mysqli_fetch_array($result);
+	if(isset($check)){
+	echo 'success';
+	}else{
+	echo 'failure';
+	}
 	// if ($conn->query($sql) === TRUE) {
     // header('Location: http://25.29.164.31:5501/SimpleTax.html'); 
 	// }
