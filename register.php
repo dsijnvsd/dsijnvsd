94722dc03fbd8395d6245dfd8099096242323b79
Wenhao Liu <809968@ACDM.DS.SAIT.CA>
2020-09-03 23:07:34 -0600

    0

--- register.php
+++ register.php
@@ -1,20 +1,21 @@
+  
 <html><body>
 
 <?php
 
 	$username = $_POST["name"];
 	$password = $_POST["pass"];
-	$servername = "localhost";
-	$dbusername = "root";
-	$db = "cprg251";
-	$dbPassword = "123456";
-	
+	$servername = "192.168.0.31";
+	$dbusername = "admin";
+	$db = "testing";
+	$dbPassword = '20200101';
+	$address = '';
 	$conn = mysqli_connect($servername, $dbusername,$dbPassword, $db);
 	if ($conn->connect_error) {
 		echo "Connection Error";
 	}
 
-	$checkpassword = "SELECT password from testing.customers where name=$username";
+	$sql = "INSERT INTO login (name, address, password) VALUES ($username, $address,$password)";
 
 	if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
@@ -22,12 +23,7 @@
 	else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 	}
-		// if($checkpassword === $password){
-		// 	echo "Your password is correct";
-		// }
-		// else{
-		// 	echo "Please try again.";
-		// }
+
 $conn->close();
 
 ?>
